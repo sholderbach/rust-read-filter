7a2acc011b1891e85e47ccae8e60080dc6f02e12
sholderbach <ho.steve@web.de>
2020-07-27 19:44:54 +0200

    Refactor and add initial config parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,103 @@
+pub mod handling {
+    //! Contains Traits to unwrap without panic but a deliberate process exit.
+    //! Behavior regarding teardown destructors might need to be considered as it calls std::process:exit
+    use std::process;
+    /// Lets the program exit with exit code one based on Result<T,E> with desired messages to stderr
+    pub trait GracefulResult<T> {
+        fn unwrap_graceful(self) -> T;
+        fn unwrap_messageful(self, msg: &str) -> T;
+        fn unwrap_formatful(self, fmt: &str) -> T;
+    }
+
+    impl<T, E: std::fmt::Display> GracefulResult<T> for Result<T, E> {
+        /// If Err(e) print e to stderr and exit with 1.
+        fn unwrap_graceful(self) -> T {
+            match self {
+                Err(e) => {
+                    eprintln!("{}", e);
+                    process::exit(1);
+                }
+                Ok(x) => x,
+            }
+        }
+        /// If Err(_) print msg to stderr and exit with 1.
+        fn unwrap_messageful(self, msg: &str) -> T {
+            match self {
+                Err(_) => {
+                    eprintln!("{}", msg);
+                    process::exit(1);
+                }
+                Ok(x) => x,
+            }
+        }
+        /// If Err(e) print "{msg}: {e}" to stderr and exit with 1.
+        fn unwrap_formatful(self, fmt: &str) -> T {
+            match self {
+                Err(e) => {
+                    eprintln!("{}: {}", fmt, e);
+                    process::exit(1);
+                }
+                Ok(x) => x,
+            }
+        }
+    }
+
+    /// Shortcut to fail with exit code 1 and a custom message for empty options
+    pub trait GracefulOption<T> {
+        fn unwrap_graceful(self, msg: &str) -> T;
+    }
+
+    impl<T> GracefulOption<T> for Option<T> {
+        /// If None print msg to stderr and exit with 1.
+        fn unwrap_graceful(self, msg: &str) -> T {
+            match self {
+                None => {
+                    eprintln!("{}", msg);
+                    process::exit(1);
+                }
+                Some(x) => x,
+            }
+        }
+    }
+}
+pub mod jsonconf {
+    use serde::Deserialize;
+    use std::fs::File;
+
+    #[derive(Deserialize)]
+    pub struct FilterConf {
+        pub left_flank: String,
+        pub right_flank: String,
+        pub content_length: i32,
+        pub expect_begin: i32,
+        pub tolerance: i32,
+        pub qual_peak: Option<u8>,
+        pub qual_mean: Option<u8>,
+    }
+
+    pub fn load_json_config(json_path: &str) -> Result<FilterConf, Box<dyn std::error::Error>> {
+        let reader = File::open(json_path)?;
+        let res: FilterConf = serde_json::from_reader(reader)?;
+        Ok(res)
+    }
+}
 #[cfg(test)]
 mod test {
-    #[test]
-    fn test_filter() {
+    use super::jsonconf;
 
+    #[test]
+    fn test_json_type() {
+        let js_str: &'static str = r#"{
+      "left_flank": "AGGGCCAG",
+      "right_flank": "GCCCAGGC",
+      "content_length": 27,
+      "expect_begin": 36,
+      "tolerance": 8,
+      "qual_peak":20,
+      "qual_mean":30
+  }"#;
+
+        let result: jsonconf::FilterConf = serde_json::from_str(js_str).unwrap();
+        assert_eq!(result.qual_mean.unwrap(), 30u8);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use bio::io::fastq;
+use read_filter::handling::{GracefulOption, GracefulResult};
+use read_filter::jsonconf;
+use std::error::Error;
 // TODO: Decide on json crate for config file
 
 #[macro_use]
@@ -9,8 +12,47 @@ fn main() {
     const OUT_ENDING: &str = ".processed.tsv";
     const RR_ENDING: &str = ".readreport.tsv";
     const QC_ENDING: &str = ".quality.tsv";
-    // Specify CLI
-    let matches = clap_app!(readfilter =>
+
+    let cfg = ProgConfig::from_cli().unwrap_graceful();
+    // Same as before...
+    let infile = std::path::Path::new(&cfg.infile);
+    let bname = infile
+        .file_name()
+        .unwrap_graceful("Input needs to be a file");
+    let oname = String::from(bname.to_str().unwrap()) // Can this be considered a given?
+        .replace(".fastq.gz", OUT_ENDING)
+        .replace(".txt.gz", OUT_ENDING)
+        .replace(".fastq", OUT_ENDING);
+    let outdir = std::path::Path::new(&cfg.outdir);
+    let outfile = outdir.join(oname);
+    println!("{}", outfile.to_str().unwrap());
+
+    let (reader, _) = niffler::from_path(infile).unwrap_formatful("Invalid input path!");
+    let fq_reader = fastq::Reader::new(reader);
+    for record in fq_reader.records() {
+        let record = record.expect("Invalid fastq");
+        println!("{}", record.id());
+    }
+}
+
+struct ProgConfig {
+    infile: String,
+    outdir: String,
+    rr_required: bool,
+    qc_required: bool,
+    left_flank: String,
+    right_flank: String,
+    insert_length: i32,
+    expected_start: i32,
+    position_tolerance: i32,
+    min_peak_qual: u8,
+    min_mean_qual: u8,
+}
+
+impl ProgConfig {
+    fn from_cli() -> Result<ProgConfig, Box<dyn Error>> {
+        // Specify CLI
+        let matches = clap_app!(readfilter =>
         (version: "0.1")
         (author: "Stefan Holderbach")
         (about: "Read filter for amplicon sequencing with a defined region")
@@ -21,27 +63,34 @@ fn main() {
         (@arg qc_report: -q --("qc-report") "Also output a table with overall QC information")
         (@arg debug: -d ... "Sets the level of debugging information")
     ).get_matches();
+        // Unpack arguments
+        let infile = matches
+            .value_of("INPUT")
+            .unwrap_graceful("Missing inputfile");
+        let outdir = matches
+            .value_of("OUTPUT")
+            .unwrap_graceful("Missing output directory");
+        let config_file = matches
+            .value_of("CONFIG")
+            .unwrap_graceful("Missing config file");
+        let rr_required = matches.is_present("read_report");
+        let qc_required = matches.is_present("qc_report");
 
-    // Unpack arguments
-    let infile = matches.value_of("INPUT").unwrap();
-    let outdir = matches.value_of("OUTPUT").unwrap();
-    let config_file = matches.value_of("CONFIG").unwrap();
-    let rr_required = matches.is_present("read_report");
-    let qc_required = matches.is_present("qc_report");
-    // Same as before...
-    let infile = std::path::Path::new(infile);
-    let bname = infile.file_name().expect("Input needs to be a file");
-    let oname = String::from(bname.to_str().unwrap()).replace(".fastq.gz", OUT_ENDING)
-                                                     .replace(".txt.gz", OUT_ENDING)
-                                                     .replace(".fastq", OUT_ENDING);
-    let outdir = std::path::Path::new(outdir);
-    let outfile = outdir.join(oname);
-    println!("{}", outfile.to_str().unwrap());
+        let json_config =
+            jsonconf::load_json_config(config_file).unwrap_formatful("While parsing configuration");
 
-    let (reader, _) = niffler::from_path(infile).expect("Invalid input path!");
-    let fq_reader = fastq::Reader::new(reader);
-    for record in fq_reader.records() {
-        let record = record.expect("Invalid fastq");
-        println!("{}", record.id());
+        Ok(ProgConfig {
+            infile: infile.to_string(),
+            outdir: outdir.to_string(),
+            rr_required,
+            qc_required,
+            left_flank: json_config.left_flank,
+            right_flank: json_config.right_flank,
+            insert_length: json_config.content_length,
+            expected_start: json_config.expect_begin,
+            position_tolerance: json_config.tolerance,
+            min_peak_qual: json_config.qual_peak.unwrap_or(0), // TODO: Maybe change the config type to accept options
+            min_mean_qual: json_config.qual_mean.unwrap_or(0),
+        })
     }
 }
